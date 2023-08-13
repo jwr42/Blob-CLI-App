af4dc5b462dc09c6626b0a09388c09d1cafa4110
jonathan <jonathan.w.roberts@pm.me>
2023-08-13 19:28:53 +0100

    Added prediction input and a score dict

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ pause = 2 # time to pause between steps in seconds
 time.sleep(pause)
 
 # player parameters
-num_players = 4
-num_pots_in_round = 5
+num_players = 2
+num_pots_in_round = 3
 
 # check if this is a valid combo
 if num_players * num_pots_in_round > 52:
@@ -38,7 +38,7 @@ def get_hand(deck, num_pots_in_round):
     return hand
 
 # get cards for each player
-for i in range(num_players):
+for player_id in turn_order:
     hands.append(get_hand(deck, num_pots_in_round))
 
 # turn a tuple into a human readable string
@@ -48,19 +48,35 @@ def get_card_string(card_tuple):
     values_map = {0:"2", 1:"3", 2:"4", 3:"5", 4:"6", 5:"7", 6:"8", 7:"9", 8:"10", 9:"J", 10:"Q", 11:"K", 12:"A"}
     return values_map[card_tuple[1]]+suit_map[card_tuple[0]]
 
-
 for player_id in turn_order:
     print(f"Player {player_id+1}'s cards: ", end="")
     for card_id in range(len(hands[player_id])):
         print(f"{get_card_string(hands[player_id][card_id])} ", end="")
-    print() # jumps to a new line for the next player
+    print() # jumps to a new line
 
-# TODO: ask players how many pots they think they'll win
+# gather predictions from each of the players
+predictions = []
+print()
+for player_id in turn_order:
+    selection_id = input(f"Player {player_id+1}, please predict how many times you'll win this round: [enter a number] ")
+    # check for non-integer values
+    try:
+        selection_id = int(selection_id)
+    except ValueError:
+        selection_id = np.random.randint(num_pots_in_round)
+        print(f"Invalid entry, random choice of {selection_id} has been made instead - good luck!")
+    if (selection_id < 0 or selection_id >= num_pots_in_round):
+        selection_id = np.random.randint(num_pots_in_round)
+        print(f"Invalid entry, random choice of {selection_id} has been made instead - good luck!")
+    predictions.append(selection_id)
+
+# store a count of the number of pots won, starting with a zero for every player
+pots_won = {key:0 for key in range(num_players)}
 
 for num_pot in range(num_pots_in_round):
 
     print()
-    print(f"There are {num_pots_in_round - num_pot} points left to win")
+    print(f"There are {num_pots_in_round - num_pot} pots left to win")
     
     # we skip for the first loop as they already know what cards they have
     if num_pot != 0:
@@ -68,7 +84,8 @@ for num_pot in range(num_pots_in_round):
             print(f"Player {player_id+1}'s cards: ", end="")
             for card_id in range(len(hands[player_id])):
                 print(f"{get_card_string(hands[player_id][card_id])} ", end="")
-            print() # jumps to a new line for the next player
+            print() # jumps to a new line
+    print()
 
     # create an empty list for players to add their cards to the plot
     pot = []
@@ -80,16 +97,17 @@ for num_pot in range(num_pots_in_round):
         # (players must follow suit if they have a card in their hand matching the suit of the first card added to the pot)
         if len(hands[0]) > 1: # check if there's more than 1 card left in the players' hands
             player_suits = [x[0] for x in hands[player_id]] # list of suits in the player's hand
-            pot_suit = pot[0][0] # suit set by the first card in the pot
-            if pot_suit in player_suits:
-                must_follow_suit = True
-                valid_card_ids = []
-                print(f"Player {player_id+1}, you must follow suit therefore you may only select card number: ", end="")
-                for card_id in range(len(hands[player_id])):
-                    if hands[player_id][card_id][0] == pot_suit:
-                        print(f"{card_id+1}", end=" ")
-                        valid_card_ids.append(card_id)
-                print()
+            if len(pot) > 0:
+                pot_suit = pot[0][0] # suit set by the first card in the pot
+                if pot_suit in player_suits:
+                    must_follow_suit = True
+                    valid_card_ids = []
+                    print(f"Player {player_id+1}, you must follow suit therefore you may only select card number: ", end="")
+                    for card_id in range(len(hands[player_id])):
+                        if hands[player_id][card_id][0] == pot_suit:
+                            print(f"{card_id+1}", end=" ")
+                            valid_card_ids.append(card_id)
+                    print()
         
         # If the player has only one card, play that card, otherwise ask which card the player would like to select
         if len(hands[player_id]) == 1:
@@ -122,7 +140,7 @@ for num_pot in range(num_pots_in_round):
                 print("Did not follow suit! A random valid card has been selected")
         
         # output the player card selection
-        if len(hands[player_id]) != 0:
+        if len(hands[player_id]) > 1: # no need to announce of the final turn
             print(f"Player {player_id+1} selected {get_card_string(hands[player_id][selection_id])}")
 
         # add card from the player#s hand to the pot
@@ -143,25 +161,25 @@ for num_pot in range(num_pots_in_round):
         if ((pot[card_id][0] == winning_card[0]) and (pot[card_id][1] > winning_card[1])):
             winning_card = pot[card_id]
             winning_card_id = card_id
-            print("bing")
         # if card has hearts as suit and winning card does not, first card is beat
         if ((pot[card_id][0] == 1) and (winning_card[0] != 1)):
             winning_card = pot[card_id]
             winning_card_id = card_id
-            print("bong")
         # if both cards have hearts as suit but card is higher value, first card is beat
         if ((pot[card_id][0] == 1) and (winning_card[0] == 1) and (pot[card_id][1] > winning_card[1])):
             winning_card = pot[card_id]
             winning_card_id = card_id
-            print("clang")
     
     # card position in the pot is determined by the turn order
     winning_player_id = turn_order[winning_card_id]
     print(f"Player {winning_player_id+1} wins the pot")
 
+    # add win to the score counter dict
+    pots_won[winning_player_id] += 1
+
     # pause before the next pot is created
     time.sleep(pause)
 
-    # update the turn order
-    turn_order.insert(0,turn_order[-1])
-    turn_order.pop(-1)
+    # update the turn order TODO: update so that the player to win the round goes first
+    turn_order.append(turn_order[0])
+    turn_order.pop(0)
